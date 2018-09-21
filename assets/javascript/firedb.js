d14361f8904053ff27de1f2216dfe68c90ae840e
Jesse W. Salmon <j_aw_s@icloud.com>
2018-09-21 05:37:17 -0700

    add files for google map god, firebase code, and money code. rename prj file to tickmster

---
+++ assets/javascript/firedb.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyCwBkD1a9nm9CqM2w15tCv6Tf0HvJcmNaw",
+    authDomain: "project1-14a42.firebaseapp.com",
+    databaseURL: "https://project1-14a42.firebaseio.com",
+    projectId: "project1-14a42",
+    storageBucket: "",
+    messagingSenderId: "950709385156"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
