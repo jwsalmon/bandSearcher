26c5d6d62d5be6941acf177f6b0cd49d7b695f6f
Jesse W. Salmon <j_aw_s@icloud.com>
2018-09-22 13:25:18 -0700

    pull info from event table and create arrayfor senting to firebase

--- assets/javascript/tickmster.js
+++ assets/javascript/tickmster.js
@@ -3,7 +3,7 @@ var artist = "";
 var venueCity = "";
 var venueName = "";
 var radius = "";
-var priceMin, priceMax;
+var priceMin, priceMax, venueDate;
 //search events on artist only
 function searchBands(artist) {
     var apikey = 'EEAFHQsgoEyLU0ADnSWmpE4DA5UOOR8G'
@@ -19,6 +19,7 @@ function searchBands(artist) {
             for (var i = 0; i < events.length; i++) {
                 venueName = events[i]._embedded.venues[0].name;
                 venueCity = events[i]._embedded.venues[0].city.name;
+                venueDate = events[i].dates.start.localDate
 
                 if (events[i].priceRanges != undefined) {
                     priceMin = events[i].priceRanges[0].min;
@@ -33,16 +34,17 @@ function searchBands(artist) {
                     $("<td>").text(artist),
                     $("<td>").text(venueName),
                     $("<td>").text(venueCity),
+                    $("<td>").text(venueDate),
                     $("<td>").text(priceMin),
                     $("<td>").text(priceMax)
                 );
-                newRow.attr("data-lat",events[i]._embedded.venues[0].location.latitude);
-                newRow.attr("data-long",events[i]._embedded.venues[0].location.longitude);
+                newRow.attr("data-lat", events[i]._embedded.venues[0].location.latitude);
+                newRow.attr("data-long", events[i]._embedded.venues[0].location.longitude);
                 newRow.addClass("eventEntry");
                 // Do other things.
                 $("#bandEvents-Table > tbody").append(newRow);
-                $(".eventEntry").click(addToInten);
             }
+            $(".eventEntry").click(addToInten);
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
@@ -65,6 +67,7 @@ function searchBands(artist, frmDate, toDate) {
             for (var i = 0; i < events.length; i++) {
                 venueName = events[i]._embedded.venues[0].name;
                 venueCity = events[i]._embedded.venues[0].city.name;
+                venueDate = events[i].dates.start.localDate
 
                 if (events[i].priceRanges != undefined) {
                     priceMin = events[i].priceRanges[0].min;
@@ -79,16 +82,17 @@ function searchBands(artist, frmDate, toDate) {
                     $("<td>").text(artist),
                     $("<td>").text(venueName),
                     $("<td>").text(venueCity),
+                    $("<td>").text(venueDate),
                     $("<td>").text(priceMin),
                     $("<td>").text(priceMax)
                 );
-                newRow.attr("data-lat",events[i]._embedded.venues[0].location.latitude);
-                newRow.attr("data-long",events[i]._embedded.venues[0].location.longitude);
+                newRow.attr("data-lat", events[i]._embedded.venues[0].location.latitude);
+                newRow.attr("data-long", events[i]._embedded.venues[0].location.longitude);
                 newRow.addClass("eventEntry");
                 // Do other things.
                 $("#bandEvents-Table > tbody").append(newRow);
-                $(".eventEntry").click(addToInten);
             }
+            $(".eventEntry").click(addToInten);
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
@@ -113,6 +117,7 @@ function searchBands(artist, frmDate, toDate, location, radius) {
 
                 venueName = events[i]._embedded.venues[0].name;
                 venueCity = events[i]._embedded.venues[0].city.name;
+                venueDate = events[i].dates.start.localDate
 
                 if (events[i].priceRanges != undefined) {
                     priceMin = events[i].priceRanges[0].min;
@@ -124,19 +129,20 @@ function searchBands(artist, frmDate, toDate, location, radius) {
                 }
                 // Create the new row
                 var newRow = $("<tr>").append(
-                    $("<td>").text(artistName),
+                    $("<td>").text(artist),
                     $("<td>").text(venueName),
                     $("<td>").text(venueCity),
+                    $("<td>").text(venueDate),
                     $("<td>").text(priceMin),
                     $("<td>").text(priceMax)
                 );
-                newRow.attr("data-lat",events[i]._embedded.venues[0].location.latitude);
-                newRow.attr("data-long",events[i]._embedded.venues[0].location.longitude);
+                newRow.attr("data-lat", events[i]._embedded.venues[0].location.latitude);
+                newRow.attr("data-long", events[i]._embedded.venues[0].location.longitude);
                 newRow.addClass("eventEntry");
                 // Do other things.
                 $("#bandEvents-Table > tbody").append(newRow);
-                $(".eventEntry").click(addToInten);
             }
+            $(".eventEntry").click(addToInten);
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
@@ -156,9 +162,10 @@ function searchBands(artist, location, radius) {
             console.log(json);
             var events = json._embedded.events;
             for (var i = 0; i < events.length; i++) {
-                 venueName = events[i]._embedded.venues[0].name;
-                 venueCity = events[i]._embedded.venues[0].city.name;
-                 
+                venueName = events[i]._embedded.venues[0].name;
+                venueCity = events[i]._embedded.venues[0].city.name;
+                venueDate = events[i].dates.start.localDate
+
                 if (events[i].priceRanges != undefined) {
                     priceMin = events[i].priceRanges[0].min;
                     priceMax = events[i].priceRanges[0].max;
@@ -172,16 +179,17 @@ function searchBands(artist, location, radius) {
                     $("<td>").text(artist),
                     $("<td>").text(venueName),
                     $("<td>").text(venueCity),
+                    $("<td>").text(venueDate),
                     $("<td>").text(priceMin),
                     $("<td>").text(priceMax)
                 );
-                newRow.attr("data-lat",events[i]._embedded.venues[0].location.latitude);
-                newRow.attr("data-long",events[i]._embedded.venues[0].location.longitude);
+                newRow.attr("data-lat", events[i]._embedded.venues[0].location.latitude);
+                newRow.attr("data-long", events[i]._embedded.venues[0].location.longitude);
                 newRow.addClass("eventEntry");
                 // Do other things.
                 $("#bandEvents-Table > tbody").append(newRow);
-                $(".eventEntry").click(addToInten);
             }
+            $(".eventEntry").click(addToInten);
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
@@ -191,29 +199,29 @@ function searchBands(artist, location, radius) {
 }
 
 function searchForBand() {
-    
+
     artist = $("#bandName").val();
     var locationZip = $("#location").val();
     var searchRange = $("#radius").val();
     var frmDate = $("#fromDate").val();
     var toDate = $("#toDate").val();
 
-    console.log("you enter a date range from: " + frmDate.toString() + " to: " + toDate.toString()); 
+    console.log("you enter a date range from: " + frmDate.toString() + " to: " + toDate.toString());
     if ((locationZip === "") && (searchRange === "") && (frmDate === "")) {
         searchBands(artist);
     }
-    else if((locationZip === "") && (searchRange === "") && (frmDate !== "")){
+    else if ((locationZip === "") && (searchRange === "") && (frmDate !== "")) {
         frmDate += "T00:00:00z";
         toDate += "T23:59:59z";
         searchBands(artist, frmDate, toDate);
     }
-    else if ((locationZip !== "") && (searchRange !== "") && (frmDate === "")){
+    else if ((locationZip !== "") && (searchRange !== "") && (frmDate === "")) {
         searchBands(artist, location, radius)
     }
 
     else {
         frmDate += "T00:00:00z";
         toDate += "T23:59:59z";
-        searchBands(artist, frmDate, toDate, location, radius); 
+        searchBands(artist, frmDate, toDate, location, radius);
     }
 }
