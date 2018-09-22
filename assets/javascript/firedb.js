1503679dadb7091ba8e33b204be4fa6be5c27db2
Jesse W. Salmon <j_aw_s@icloud.com>
2018-09-22 00:51:29 -0700

    firebase code ready for hook up to divs & tables. ticket master code for hook up to tables. google map code ready for hook up to addresses

--- assets/javascript/firedb.js
+++ assets/javascript/firedb.js
@@ -5,27 +5,53 @@ var config = {
     projectId: "project1-14a42",
     storageBucket: "",
     messagingSenderId: "950709385156"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  function saveBandToIten(user,artist,venueName,venueCity,priceMin,priceMax){
-    
-        user = {
+function saveBandToIten(user, artist, venueName, venueCity, priceMin, priceMax,venueLat,venueLong) {
+
+    user = {
         artist: artist,
         venueName: venueName,
         venueCity: venueCity,
         priceMax: priceMax,
-        priceMin: priceMin
+        priceMin: priceMin,
+        lat: venueLat,
+        long: venuelong
 
-      };
-    
-      // Uploads artist data to user in database
-      database.ref().push(user);
-  }
+    };
 
-  //add code to load iteniary from firebase for given user
-  function loadIten(user){
-      
-  }
+    // Uploads artist data to user in database
+    database.ref().push(user);
+}
+
+//add code to load iteniary from firebase for given user
+function loadIten(user) {
+    database.ref(user).once('value').then(function (snapshot) {
+        var userIten = snapshot.val();
+        for (var i = 0; i < userIten.length; i++) {
+            var artist = userIten[i].artist;
+            var venueName = userIten[i].venueName;
+            var venueCity = userIten[i].venueCity;
+            var priceMax = userIten[i].priceMax;
+            var priceMin = userIten[i].priceMin;
+            var lat = userIten[i].venueLat;
+            var long = userIten[i].venueLong;
+
+            var newRow = $("<tr>").append(
+                $("<td>").text(artist),
+                $("<td>").text(venueName),
+                $("<td>").text(venueCity),
+                $("<td>").text(priceMin),
+                $("<td>").text(priceMax)
+            );
+            newRow.attr("data-lat",lat);
+            newRow.attr("data-long",long);
+            // Do other things.
+            $("#bandIteniary-Table > tbody").append(newRow);
+        }
+        
+    });
+}
--- assets/javascript/googmap.js
+++ assets/javascript/googmap.js
@@ -1,41 +1,27 @@
-//this is sample code need to modify
-var $detailWin,
-    dialogInitialized,
-    map;
-
-function getDialogContent() {
-    if (dialogInitialized) return;
-    dialogInitialized = true;//Well, at least initializing.
-    $.get('p2.php').done(function (html) {
-        $detailWin.html(html);
-        var cord = new google.maps.LatLng(28.545078, -81.377196);
-        var mapOptions = {
-            zoom: 15,
-            center: cord,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: cord,
-            map: map,
-            title: 'Test'
+var lat, long;
+$(function () {
+    $(".btnShow").click(function () {
+        lat = $(this).attr("data-latitude");
+        long = $(this).attr("data-longitude");
+        $("#dialog").dialog({
+            modal: true,
+            title: "Google Map",
+            width: 600,
+            hright: 450,
+            buttons: {
+                Close: function () {
+                    $(this).dialog('close');
+                }
+            },
+            open: function () {
+                var mapOptions = {
+                    center: new google.maps.LatLng(lat, long),
+                    zoom: 18,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                var map = new google.maps.Map($("#dvMap")[0], mapOptions);
+            }
         });
-    }).error(function (jqXHR, textStatus, errorThrown) {
-        $detailWin.text(textStatus);
     });
-}
-
-$detailWin = $("#detailWin").dialog({
-    autoOpen: false,
-    modal: true,
-    width: 700,
-    height: 600,
-    show: "fade",
-    close: "fade",
-    open: getDialogContent
 });
 
-//this will be moved to logic and triggered from click of venuename
-$("#cc").on('click', function () {
-    $detailWin.dialog("open");
-});
